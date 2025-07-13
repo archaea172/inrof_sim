d5fab3902a38a00f2934a90e0337571196f213e9
aky <akiyugata271@gmail.com>
2025-07-13 18:17:47 +0900

    add function

--- src/pursuit.cpp
+++ src/pursuit.cpp
@@ -9,10 +9,54 @@
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
+class PursuitControler : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+    // constructor
+    PursuitControler()
+    : rclcpp_lifecycle::LifecycleNode(std::string("pursuit_controler"))
+    {
+
+    }
+    //desconstructor
+    ~PursuitControler()
+    {
+
+    }
+private:
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state)
+    {
+
+    }
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &state)
+    {
+
+    }
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state)
+    {
+
+    }
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state)
+    {
+
+    }
+    CallbackReturn on_error(const rclcpp_lifecycle::State &state)
+    {
+
+    }
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state)
+    {
+
+    }
+};
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-
+    std::shared_ptr<PursuitControler> node = std::make_shared<PursuitControler>();
+    rclcpp::spin(node->get_node_base_interface());
     rclcpp::shutdown();
     return 0;
 }
