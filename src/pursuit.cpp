da0ee36b108fb89ac1df4d212765529fe65e344b
aky <akiyugata271@gmail.com>
2025-07-13 18:07:38 +0900

    add header

---
+++ src/pursuit.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
