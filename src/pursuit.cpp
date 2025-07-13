bb55484a845020776cddfcf7bbdaa1967748261f
aky <akiyugata271@gmail.com>
2025-07-13 18:08:59 +0900

    add namespace rcl progress

--- src/pursuit.cpp
+++ src/pursuit.cpp
@@ -6,8 +6,13 @@
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
 int main(int argc, char *argv[])
 {
-    
+    rclcpp::init(argc, argv);
+
+    rclcpp::shutdown();
     return 0;
 }
