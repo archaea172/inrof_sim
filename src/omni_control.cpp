b7691bcbb599791a81c889cb51ec16655c799451
aky <akiyugata271@gmail.com>
2025-07-13 14:52:46 +0900

    add on_configure

--- src/omni_control.cpp
+++ src/omni_control.cpp
@@ -18,13 +18,28 @@ class OmniControler : public rclcpp_lifecycle::LifecycleNode
 public:
     using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
+    // constructor
     OmniControler()
     : rclcpp_lifecycle::LifecycleNode(std::string("omni_controler"))
     {
         
     };
+
+    // desconstructor
     ~OmniControler()
     {
         RCLCPP_INFO(this->get_logger(), "finish");
     }
+
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state)
+    {
+        joint_pub = this->create_publisher<std_msgs::msg::Float32>(
+            std::string("wheel_joint"), rclcpp::SystemDefaultsQoS()
+        );
+        return CallbackReturn::SUCCESS;
+    }
+
+private:
+    rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Float32>::SharedPtr joint_pub;
+
 };
