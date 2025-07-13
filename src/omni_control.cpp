85ab90a3e6604b11e9e4cda898eae82c11815a29
aky <akiyugata271@gmail.com>
2025-07-13 13:22:19 +0900

    add constructor and descontructor

--- src/omni_control.cpp
+++ src/omni_control.cpp
@@ -9,3 +9,22 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "std_msgs/msg/float32.hpp"
 
+using std::placeholders::_1;
+
+using namespace std::chrono_literals;
+
+class OmniControler : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+    OmniControler()
+    : rclcpp_lifecycle::LifecycleNode(std::string("omni_controler"))
+    {
+        
+    };
+    ~OmniControler()
+    {
+        RCLCPP_INFO(this->get_logger(), "finish");
+    }
+};
