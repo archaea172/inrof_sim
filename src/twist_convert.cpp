da66c4b3e9992d5fe001f857ecb0be144b88bda4
aky <akiyugata271@gmail.com>
2025-07-16 16:58:54 +0900

    動かせた
    次はこれをまとめたlaunchファイルを作りたい

--- src/twist_convert.cpp
+++ src/twist_convert.cpp
@@ -29,4 +29,79 @@ public:
     {
         RCLCPP_INFO(this->get_logger(), "finish");
     }
+
+private:
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state)
+    {
+        robot_twist_publisher = this->create_publisher<geometry_msgs::msg::Twist>(
+            std::string("cmd_vel_robot"), rclcpp::SystemDefaultsQoS()
+        );
+        return CallbackReturn::SUCCESS;
+    }
+
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &state)
+    {
+        // activate publisher
+        robot_twist_publisher->on_activate();
+
+        // create subscriber
+        controler_twist_subscriber = this->create_subscription<geometry_msgs::msg::Twist>(
+            std::string("cmd_vel"),
+            rclcpp::SystemDefaultsQoS(),
+            std::bind(&TwistConverter::vel_callback, this, _1)
+        );
+        pub_timer = this->create_wall_timer(0.001s, std::bind(&TwistConverter::timer_callback, this));
+        return CallbackReturn::SUCCESS;
+    }
+
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state)
+    {
+        // deactivate publisher
+        robot_twist_publisher->on_deactivate();
+        // reset subscriber
+        controler_twist_subscriber.reset();
+        pub_timer.reset();
+        return CallbackReturn::SUCCESS;
+    }
+
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state)
+    {
+        // reset publisher
+        robot_twist_publisher.reset();
+        return CallbackReturn::SUCCESS;
+    }
+
+    CallbackReturn on_error(const rclcpp_lifecycle::State &state)
+    {
+        RCLCPP_INFO(this->get_logger(), "on error!", state.label().c_str());
+        return CallbackReturn::SUCCESS;
+    }
+
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state)
+    {
+        return CallbackReturn::SUCCESS;
+    }
+
+    void vel_callback(const geometry_msgs::msg::Twist::SharedPtr rxdata)
+    {
+
+    }
+
+    void timer_callback()
+    {
+
+    }
+
+    rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::Twist>::SharedPtr robot_twist_publisher;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr controler_twist_subscriber;
+    rclcpp::TimerBase::SharedPtr pub_timer;
 };
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    std::shared_ptr<TwistConverter> node = std::make_shared<TwistConverter>();
+    rclcpp::spin(node->get_node_base_interface());
+    rclcpp::shutdown();
+    return 0;
+}
