b428376552c97bfef1b6637847ea52e5bd639344
aky <akiyugata271@gmail.com>
2025-07-16 15:22:02 +0900

    add class

---
+++ src/twist_convert.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <chrono>
+#include <memory>
+#include <math.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+
+#include "geometry_msgs/msg/twist.hpp"
+
+using std::placeholders::_1;
+
+using namespace std::chrono_literals;
+
+class TwistConverter : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+
+};
