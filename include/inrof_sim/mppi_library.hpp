0dbc6ca61d3f52a9d761a1b0a7819de9d9f3af44
aky <akiyugata271@gmail.com>
2025-07-22 16:53:50 +0900

    add comment

--- include/inrof_sim/mppi_library.hpp
+++ include/inrof_sim/mppi_library.hpp
@@ -10,11 +10,16 @@ public:
     ~MppiControl();
 
 private:
+    /*declare value begin*/
     int input_dim_;
+    /*declare value end*/
 
+    /*estimate func begin*/
     double estimate_ref();
     double estimate_smooth();
+    /*estimate func end*/
     Eigen::VectorXd model(Eigen::MatrixXd input_array, Eigen::VectorXd init_state);
+
     double clamp(const double value, const double max_value, const double min_value);
 };
 
