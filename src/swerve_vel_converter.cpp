959f762823a5ce9aca71d11da22a8457ecbc5417
aky <akiyugata271@gmail.com>
2025-08-01 15:49:51 +0900

    add constructor

--- src/swerve_vel_converter.cpp
+++ src/swerve_vel_converter.cpp
@@ -1,2 +1,11 @@
 #include "swerve_vel_converter.hpp"
 #include "rclcpp/rclcpp.hpp"
+
+SwerveVelConverter::SwerveVelConverter()
+: rclcpp_lifecycle::LifecycleNode(std::string("swerve_vel_converter"))
+{
+    /*parameter declare begin*/
+    this->declare_parameter<double>("robot_radius", 1.0);
+    this->declare_parameter<double>("wheel_radius", 0.1);
+    /*parameter declare end*/
+}
