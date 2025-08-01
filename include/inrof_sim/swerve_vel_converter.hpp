4e3ee7d0349f3866d59afe63f8c50c4e0c37e87c
aky <akiyugata271@gmail.com>
2025-08-01 14:40:20 +0900

    add swerve vel converter

---
+++ include/inrof_sim/swerve_vel_converter.hpp
@@ -0,0 +1,11 @@
+#ifndef SWERVE_VEL_CONVERTER_HPP
+#define SWERVE_VEL_CONVERTER_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+#endif
