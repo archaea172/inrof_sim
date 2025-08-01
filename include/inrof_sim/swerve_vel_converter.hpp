d93cad8aaeb9372a174c3c18cedd676da2988dc6
aky <akiyugata271@gmail.com>
2025-08-01 14:49:36 +0900

    add class func

--- include/inrof_sim/swerve_vel_converter.hpp
+++ include/inrof_sim/swerve_vel_converter.hpp
@@ -5,7 +5,28 @@
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 
+#include "std_msgs/msg/float64.hpp"
+
 using std::placeholders::_1;
 using namespace std::chrono_literals;
+
+class SwerveVelConverter : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    /*type define begin*/
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+    /*type define end*/
+
+    /*class func begin*/
+    SwerveVelConverter();
+    ~SwerveVelConverter();
+    /*class func end*/
+
+private:
+    /*node value define begin*/
+    rclcpp::TimerBase::SharedPtr cal_timer;
+    rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Float64>::SharedPtr wheel0_vel;
+    /*node value define end*/
+};
 
 #endif
