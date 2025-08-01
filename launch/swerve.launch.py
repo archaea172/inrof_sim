c32be6db9394e9c34d456543211c21e5b08de06b
aky <akiyugata271@gmail.com>
2025-08-01 23:25:35 +0900

    add sim

--- launch/swerve.launch.py
+++ launch/swerve.launch.py
@@ -7,17 +7,19 @@ import os
 
 
 def generate_launch_description():
+    ld = LaunchDescription()
     world_file_path = PathJoinSubstitution([
         get_package_share_directory('inrof_sim'),
         'worlds',
         'swerve_world.sdf'
     ])
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-            launch_arguments=[
-                ('gz_args', world_file_path)]
-        )
-    ])
+    sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+        launch_arguments=[
+            ('gz_args', world_file_path)]
+    )
+
+    ld.add_action(sim)
+    return ld
