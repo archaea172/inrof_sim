9b564bbd0b16c8f5d02b5a11062f63c771d5657b
aky <akiyugata271@gmail.com>
2025-08-01 15:53:56 +0900

    add get parameter

--- src/swerve_vel_converter.cpp
+++ src/swerve_vel_converter.cpp
@@ -8,4 +8,9 @@ SwerveVelConverter::SwerveVelConverter()
     this->declare_parameter<double>("robot_radius", 1.0);
     this->declare_parameter<double>("wheel_radius", 0.1);
     /*parameter declare end*/
+
+    /*parameter set begin*/
+    this->R = this->get_parameter("robot_radius").as_double();
+    this->r = this->get_parameter("wheel_radius").as_double();
+    /*parameter set end*/
 }
