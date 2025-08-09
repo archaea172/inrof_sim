c11cc72109c4b3ec6ac7676da9966b5990abdd9e
aky <akiyugata271@gmail.com>
2025-08-09 20:41:56 +0900

    update model

--- src/mppi_swerve.cpp
+++ src/mppi_swerve.cpp
@@ -2,5 +2,8 @@
 
 Eigen::VectorXd MppiSwerve::model(const Eigen::VectorXd &input, const Eigen::VectorXd &pre_state) override
 {
-    
+    Eigen::VectorXd post_state(this->input_dim_);
+    post_state = pre_state + this->control_cycle_*input;
+    return post_state;
 }
+
