553d856c0aed7e2c5b553a29be2682a8e1a3a261
aky <akiyugata271@gmail.com>
2025-08-09 21:44:57 +0900

    update genrate

--- src/mppi_swerve.cpp
+++ src/mppi_swerve.cpp
@@ -1,9 +1,16 @@
 #include "mppi_swerve.hpp"
 
-Eigen::VectorXd MppiSwerve::model(const Eigen::VectorXd &input, const Eigen::VectorXd &pre_state) override
+Eigen::VectorXd MppiSwerve::model(const Eigen::VectorXd &input, const Eigen::VectorXd &pre_state)
 {
     Eigen::VectorXd post_state(this->input_dim_);
     post_state = pre_state + this->control_cycle_*input;
     return post_state;
 }
 
+Eigen::MatrixXd MppiSwerve::generate_model_state(const Eigen::MatrixXd &input_array, const Eigen::VectorXd &init_state)
+{
+    Eigen::MatrixXd state_array(this->predict_horizon_, this->input_dim_);
+    state_array.row(0) = init_state;
+    for (size_t i = 1; i < (size_t)predict_horizon_; i++) state_array.row(i) = this->model(input_array.row(i), state_array.row(i-1)).transpose();
+    return state_array;
+}
